56b7342efb03615ead927ab0410115c84a859f99
Ruan,Shudan <shudan.ruan@edoc2.com>
2016-07-18 14:10:03 +0800

    Add

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,154 @@
+﻿using Data.Data.EntityModule;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    class Program
+    {
+        static readonly RedisClient redis = new RedisClient("localhost");
+        static void Main(string[] args)
+        {
+
+            redis.FlushAll();
+            InsertTestData();
+
+
+            var redisBlogs = redis.As<Blog>();
+            var blogs = redisBlogs.GetAll();//显示所有博客
+
+
+            Show_a_list_of_recent_posts_and_comments();
+
+
+
+            Console.ReadKey();
+        }
+
+
+        public static void Show_a_list_of_recent_posts_and_comments()
+        {
+            //Get strongly-typed clients
+            var redisBlogPosts = redis.As<BlogPost>();
+            var redisComments = redis.As<BlogPostComment>();
+            {
+                //To keep this example let's pretend this is a new list of blog posts
+                var newIncomingBlogPosts = redisBlogPosts.GetAll();
+
+                //Let's get back an IList<BlogPost> wrapper around a Redis server-side List.
+                var recentPosts = redisBlogPosts.Lists["urn:BlogPost:RecentPosts"];
+                var recentComments = redisComments.Lists["urn:BlogPostComment:RecentComments"];
+
+                foreach (var newBlogPost in newIncomingBlogPosts)
+                {
+                    //Prepend the new blog posts to the start of the 'RecentPosts' list
+                    recentPosts.Prepend(newBlogPost);
+
+                    //Prepend all the new blog post comments to the start of the 'RecentComments' list
+                    newBlogPost.Comments.ForEach(recentComments.Prepend);
+                }
+
+                //Make this a Rolling list by only keep the latest 3 posts and comments
+                recentPosts.Trim(0, 2);
+                recentComments.Trim(0, 2);
+
+                //Print out the last 3 posts:
+                var ss = recentPosts.GetAll();
+                var dd = recentComments.GetAll();
+            }
+        }
+        public static void InsertTestData()
+        {
+            var redisUsers = redis.As<User>();
+            var redisBlogs = redis.As<Blog>();
+            var redisBlogPosts = redis.As<BlogPost>();
+
+            var yangUser = new User { Id = redisUsers.GetNextSequence(), Name = "Eric Yang" };
+            var zhangUser = new User { Id = redisUsers.GetNextSequence(), Name = "Fish Zhang" };
+
+            var yangBlog = new Blog
+            {
+                Id = redisBlogs.GetNextSequence(),
+                UserId = yangUser.Id,
+                UserName = yangUser.Name,
+                Tags = new List<string> { "Architecture", ".NET", "Databases" },
+            };
+
+            var zhangBlog = new Blog
+            {
+                Id = redisBlogs.GetNextSequence(),
+                UserId = zhangUser.Id,
+                UserName = zhangUser.Name,
+                Tags = new List<string> { "Architecture", ".NET", "Databases" },
+            };
+            #region 微博内容
+            var blogPosts = new List<BlogPost> {
+                new BlogPost
+                {
+                    Id = redisBlogPosts.GetNextSequence(),
+                    BlogId = yangBlog.Id,
+                    Title = "Memcache",
+                    Categories = new List<string> { "NoSQL", "DocumentDB" },
+                    Tags = new List<string> {"Memcache", "NoSQL", "JSON", ".NET"} ,
+                    Comments = new List<BlogPostComment>
+                    {
+                        new BlogPostComment { Content = "First Comment!", CreatedDate = DateTime.UtcNow,},
+                        new BlogPostComment { Content = "Second Comment!", CreatedDate = DateTime.UtcNow,},
+                    }
+                },
+                new BlogPost
+                {
+                    Id = redisBlogPosts.GetNextSequence(),
+                    BlogId = zhangBlog.Id,
+                    Title = "Redis",
+                    Categories = new List<string> { "NoSQL", "Cache" },
+                    Tags = new List<string> {"Redis", "NoSQL", "Scalability", "Performance"},
+                    Comments = new List<BlogPostComment>
+                    {
+                        new BlogPostComment { Content = "First Comment!", CreatedDate = DateTime.UtcNow,}
+                    }
+                },
+                new BlogPost
+                {
+                    Id = redisBlogPosts.GetNextSequence(),
+                    BlogId = yangBlog.Id,
+                    Title = "Cassandra",
+                    Categories = new List<string> { "NoSQL", "Cluster" },
+                    Tags = new List<string> {"Cassandra", "NoSQL", "Scalability", "Hashing"},
+                    Comments = new List<BlogPostComment>
+                    {
+                        new BlogPostComment { Content = "First Comment!", CreatedDate = DateTime.UtcNow,}
+                    }
+                },
+                new BlogPost
+                {
+                    Id = redisBlogPosts.GetNextSequence(),
+                    BlogId = zhangBlog.Id,
+                    Title = "Couch Db",
+                    Categories = new List<string> { "NoSQL", "DocumentDB" },
+                    Tags = new List<string> {"CouchDb", "NoSQL", "JSON"},
+                    Comments = new List<BlogPostComment>
+                    {
+                        new BlogPostComment {Content = "First Comment!", CreatedDate = DateTime.UtcNow,}
+                    }
+                },
+            };
+            #endregion
+
+            yangUser.BlogIds.Add(yangBlog.Id);
+            yangBlog.BlogPostIds.AddRange(blogPosts.Where(x => x.BlogId == yangBlog.Id).Select(x => x.Id));
+
+            zhangUser.BlogIds.Add(zhangBlog.Id);
+            zhangBlog.BlogPostIds.AddRange(blogPosts.Where(x => x.BlogId == zhangBlog.Id).Select(x => x.Id));
+
+            redisUsers.Store(yangUser);
+            redisUsers.Store(zhangUser);
+            redisBlogs.StoreAll(new[] { yangBlog, zhangBlog });
+            redisBlogPosts.StoreAll(blogPosts);
+        }
+    }
+}
---
+++ RedisDemo/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+
+        public string UserPwd { get; set; }
+    }
+}
